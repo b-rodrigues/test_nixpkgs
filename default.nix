bc4389b039ec21efc6430cb2b7f7294f2a8a4916
Bruno Rodrigues <brodriguesco@protonmail.com>
2023-10-24 12:54:45 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/ff2a3e92a872d97c76899dd0ba250c1dcc913e6b.tar.gz") {};
  rpkgs = builtins.attrValues {
-   inherit (pkgs.rPackages) quarto;
+   inherit (pkgs.rPackages) quarto pandoc;
 };
  system_packages = builtins.attrValues {
    inherit (pkgs) R quarto;
