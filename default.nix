c632d31acb65169f9ce5e14b91e69cb83119a2fe
Bruno Rodrigues <brodriguesco@protonmail.com>
2023-12-20 08:38:13 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
- pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/06b93631a20bc9c1e73d7b5c706af12ee01922aa.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/7a9965e3a22671a815b6ee6bb1fbf6f039a4bb06.tar.gz") {};
  rpkgs = builtins.attrValues {
    inherit (pkgs.rPackages) quarto;
 };
