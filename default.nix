4228ebe78e7893a19b60ac3fef6068f25115c03b
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-04-18 16:59:28 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,15 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/master.tar.gz") {};
-# terra = pkgs.rPackages.terra.overrideAttrs (attrs: {
-#   buildInputs = attrs.buildInputs ++ pkgs.lib.optional pkgs.stdenv.isDarwin [ pkgs.curl.dev pkgs.libtiff ];
-# });
+ terra = pkgs.rPackages.terra.overrideAttrs (attrs: {
+   buildInputs = attrs.buildInputs ++ pkgs.lib.optional pkgs.stdenv.isDarwin [ pkgs.curl.dev pkgs.libtiff ];
+   configureFlags = [
+        "--with-proj-lib=${lib.getLib pkgs.proj}/lib"
+      ];
+ });
  system_packages = builtins.attrValues {
    inherit (pkgs) R;
 };
 in
  pkgs.mkShell {
-   buildInputs = [ pkgs.rPackages.sf system_packages ];
+   buildInputs = [ terra system_packages ];
 }
