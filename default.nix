99fae2f35b097d200791a7268f03964d656c11ce
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-06-04 10:35:00 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,20 @@
 let
- pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/e3f6e7a8ee005e4b3f9e8446d1958cf983a8f672.tar.gz") {};
- terra = pkgs.rPackages.terra.overrideAttrs (attrs: {
-  # buildInputs = attrs.buildInputs ++ pkgs.lib.optional pkgs.stdenv.isDarwin [ pkgs.curl.dev pkgs.libtiff ];
-   configureFlags = [
-        "--with-proj-lib=${pkgs.lib.getLib pkgs.proj}/lib"
-      ];
- });
+ pkgs = import (fetchTarball "https://github.com/r-ryantm/nixpkgs/archive/160d717aad2bcc5e68205f3def51c9d878e87095.tar.gz") {};
+ rpkgs = builtins.attrValues {
+   inherit (pkgs.rPackages) quarto reticulate;
+};
+ tex = (pkgs.texlive.combine {
+   inherit (pkgs.texlive) scheme-small amsmath;
+});
  system_packages = builtins.attrValues {
-   inherit (pkgs) R openbugs;
+   inherit (pkgs) R quarto pandoc typst;
 };
 in
  pkgs.mkShell {
-   buildInputs = [ terra system_packages ];
+   buildInputs = [ rpkgs system_packages tex];
+     shellHook = ''
+       quarto check
+       quarto render hello.qmd
+       quarto render hello_typst.qmd
+     '';
 }
