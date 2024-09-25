8088d0032655b58371a359acd4d34abfbbcca2a3
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-09-25 19:40:26 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/189e5f171b163feb7791a9118afa778d9a1db81f.tar.gz") {};
 
- sf = pkgs.rPackages.sf.overrideAttrs (attrs: {
-                configureFlags = [
-"--with-proj-lib=${pkgs.lib.getLib pkgs.proj}/lib"
-                ];
-            });
+ #sf = pkgs.rPackages.sf.overrideAttrs (attrs: {
+ #               configureFlags = [
+#"--with-proj-lib=${pkgs.lib.getLib pkgs.proj}/lib"
+#                ];
+#            });
 
  system_packages = builtins.attrValues {
    inherit (pkgs) R;
 };
 in
  pkgs.mkShell {
-   buildInputs = [ system_packages sf ];
+   buildInputs = [ system_packages pkgs.rPackages.vapour ];
     
 }
