4a87d9ce8ba8b6c70ac47ca93cf7b443bb555d04
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-10-07 09:49:05 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/189e5f171b163feb7791a9118afa778d9a1db81f.tar.gz") {};
 
- system_packages = builtins.attrValues {
+ r_pkgs = builtins.attrValues {
    inherit (pkgs.rPackages) vapour sf terra;
 };
 
@@ -10,6 +10,6 @@ let
 };
 in
  pkgs.mkShell {
-   buildInputs = [ system_packages vapour sf terra ];
+   buildInputs = [ system_packages r_pkgs ];
     
 }
