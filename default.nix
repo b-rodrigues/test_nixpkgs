831f69fa8fc9225566ace9457da02b620feefb14
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-10-07 09:47:23 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,17 +1,15 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/189e5f171b163feb7791a9118afa778d9a1db81f.tar.gz") {};
 
- vapour = pkgs.rPackages.vapour.overrideAttrs (attrs: {
-                configureFlags = [
-"--with-proj-lib=${pkgs.lib.getLib pkgs.proj}/lib"
-                ];
-            });
+ system_packages = builtins.attrValues {
+   inherit (pkgs.rPackages) vapour sf terra;
+};
 
  system_packages = builtins.attrValues {
    inherit (pkgs) R;
 };
 in
  pkgs.mkShell {
-   buildInputs = [ system_packages vapour ];
+   buildInputs = [ system_packages vapour sf terra ];
     
 }
