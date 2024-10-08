a941e75a9013e4bd828190bc08b77dd5ddac5d24
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-10-08 15:15:10 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
  pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/812fef336e2c1eacccba4e893e0338e65adb3e21.tar.gz") {};
 
  r_pkgs = builtins.attrValues {
-   inherit (pkgs.rPackages) terra;
+   inherit (pkgs.rPackages) sf terra;
 };
 
  system_packages = builtins.attrValues {
