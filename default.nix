9f4c1a88736e9fe9128b937dd87acf757559674d
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-10-17 15:21:48 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
- pkgs = import (fetchTarball "https://github.com/b-rodrigues/nixpkgs/archive/812fef336e2c1eacccba4e893e0338e65adb3e21.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/tree/master.tar.gz") {};
 
  r_pkgs = builtins.attrValues {
-   inherit (pkgs.rPackages) vapour terra;
+   inherit (pkgs.rPackages) rJava;
 };
 
  system_packages = builtins.attrValues {
