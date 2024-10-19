e6ad2300347bd01ddc4e1c23179238e4fd45d82f
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-10-19 18:31:45 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
  # march 2022
- pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/a4d921be21135b13ce6c775d08d4bf6f5df37528.tar.gz") {};
 
  r_pkgs = builtins.attrValues {
    inherit (pkgs.rPackages) rJava;
