03e4fbef80f08f0ed6c50721d1839d14ca3f6bd7
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-10-22 10:26:16 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 let
- pkgs = import (fetchTarball "https://github.com/Kupac/nixpkgs/archive/8becdcf6cc94892310f11b0e26caa5fb36b546b8.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/92feb4fd1907886f9c834628e7f6ea70bbfbe7a0.tar.gz") {};
 
-# r_pkgs = builtins.attrValues {
-#   inherit (pkgs.rPackages) later;
-#};
+ r_pkgs = builtins.attrValues {
+   inherit (pkgs.rPackages) later;
+};
 
-# system_packages = builtins.attrValues {
-#   inherit (pkgs) R;
-#};
+ system_packages = builtins.attrValues {
+   inherit (pkgs) R;
+};
 in
  pkgs.mkShell {
    buildInputs = [ pkgs.quantlib ];
