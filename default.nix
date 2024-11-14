46b6566b809dccf7f2f94c620c782a7cce61e54f
Bruno Rodrigues <brodriguesco@protonmail.com>
2024-11-14 13:22:28 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let
- pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/refs/heads/master.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/1237e5f35dd5d755f2e0279954099fc6af6e3346.tar.gz") {};
 
  r_pkgs = builtins.attrValues {
-   inherit (pkgs.rPackages) xlsx withr;
+   inherit (pkgs.rPackages) rJava withr;
 };
 
  system_packages = builtins.attrValues {
