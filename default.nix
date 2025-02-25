3ebe918fc1f18f4c24b0efaa6fa702b50e4cd72e
Bruno Rodrigues <brodriguesco@protonmail.com>
2025-02-25 10:20:39 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,14 +2,18 @@ let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f11263d6948953a83df38e232ddf773bc5d4470f.tar.gz") {};
 
  r_pkgs = builtins.attrValues {
-   inherit (pkgs.rPackages) rJava xlsx withr;
+   inherit (pkgs.rPackages) quarto;
 };
 
  system_packages = builtins.attrValues {
-   inherit (pkgs) R;
+   inherit (pkgs) R quarto typst;
 };
 in
  pkgs.mkShell {
    buildInputs = [ r_pkgs system_packages ];
-    
+   shellHook = ''
+       quarto check
+       quarto render hello.qmd
+       quarto render hello_typst.qmd
+     '';
 }
