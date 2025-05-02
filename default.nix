6bdc42fb683915eef78c5d77fdb2d43b1ed0ab66
Bruno Rodrigues <brodriguesco@protonmail.com>
2025-05-02 23:07:05 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
- pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/46bcd5008614c3128bd2482dc8d8d7aad04a5235.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8a8ad47bed7301195c77b0eaa240095871ea04fd.tar.gz") {};
 
  r_pkgs = builtins.attrValues {
    inherit (pkgs.rPackages) quarto reticulate;
